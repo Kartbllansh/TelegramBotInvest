26b7ec1c68d845ec4ea92502677360b1c559764f
Kartbllansh <movolovo93@gmail.com>
2023-06-30 22:50:10 +0300

    Day 2

--- Dispatcher/src/main/java/org/example/controller/TelegramBot.java
+++ Dispatcher/src/main/java/org/example/controller/TelegramBot.java
@@ -2,34 +2,62 @@ package org.example.controller;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
 
 @Component
+@Log4j
 //@NoArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
-    @Value("${bot.token}")
-    private   String botToken;
     @Value("${bot.name}")
     private String botName;
 
+    public TelegramBot(@Value("${bot.token}") String botToken){
+        super(botToken);
+    }
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
 
+    private UpdateConroller updateConroller;
+
+    public TelegramBot(UpdateConroller updateConroller){
+        this.updateConroller = updateConroller;
+    }
+    @PostConstruct
+    public void init(){
+        updateConroller.registerBot(this);
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
     var originalMessage = update.getMessage();
         System.out.println(originalMessage.getText());
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(originalMessage.getChatId());
+        sendMessage.setText(originalMessage.getText());
+        sendAnswerMessage(sendMessage);
     }
 
-    @Override
-    public String getBotUsername() {
-        return botName;
-    }
-    @Override
-    public String getBotToken(){
-        return botToken;
+
+
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
---
+++ Dispatcher/src/main/java/org/example/controller/UpdateConroller.java
@@ -0,0 +1,60 @@
+package org.example.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.example.utils.MessageUtils;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@Log4j
+public class UpdateConroller {
+
+    private MessageUtils messageUtils;
+
+    private TelegramBot telegramBot;
+
+    public UpdateConroller(MessageUtils messageUtils, TelegramBot telegramBot) {
+        this.messageUtils = messageUtils;
+        this.telegramBot = telegramBot;
+    }
+
+    public void registerBot(TelegramBot telegramBot){
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.hasMessage()) {
+            distributeMessagesByType(update);
+        } else {
+            log.error("Unsupported message type is received: " + update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        var message = update.getMessage();
+        if (message.hasText()) {
+            processTextMessage(update);
+        } else if (message.hasDocument()) {
+            processDocMessage(update);
+        } else if (message.hasPhoto()) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+      var sendMessage = messageUtils.generateSendMessageWithText(update, "Неподдерживаемый тип сообщений");
+      setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+}

e991d8b18009101054632e1b5b1ae9002fbc0999
Kartbllansh <movolovo93@gmail.com>
2023-07-06 17:16:49 +0300

    Day 6

--- Mail-service/src/main/java/org/example/controller/MainController.java
+++ Mail-service/src/main/java/org/example/controller/MainController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/mail")
+@RestController
+
 public class MainController {
     private final MailSenderService mailSenderService;
 

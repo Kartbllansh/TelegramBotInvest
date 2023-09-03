3aa288999eb8198860e9f0c68c0430ce9172bdee
Kartbllansh <movolovo93@gmail.com>
2023-09-03 17:03:45 +0300

    31 day

--- Dispatcher/src/main/java/org/example/Main.java
+++ Dispatcher/src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
-
 import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+
 @Log4j
 public class Main {
     public static void main(String[] args) {
